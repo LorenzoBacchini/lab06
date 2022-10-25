45f75835032bd6e390650022e279ceaed53007dc
Bacchini Lorenzo <lorenzo.bacchini4@studio.unibo.it>
2022-10-25 13:33:03 +0200

    Point 5 done

--- 63-exceptions/src/main/java/it/unibo/exceptions/arithmetic/ArithmeticService.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/arithmetic/ArithmeticService.java
@@ -17,6 +17,8 @@ import static java.lang.Math.min;
  */
 public final class ArithmeticService {
 
+    static String msg;
+
     /**
      * Multiplication.
      */
@@ -56,7 +58,8 @@ public final class ArithmeticService {
      */
     public String process() {
         if (commandQueue.isEmpty()) {
-            System.out.println("No commands sent, no result available");
+            msg = "No commands sent, no result available";
+            throw new IllegalStateException(msg);
         }
         while (commandQueue.size() != 1) {
             final var nextMultiplication = commandQueue.indexOf(TIMES);
@@ -74,18 +77,21 @@ public final class ArithmeticService {
                     : max(nextSum, nextMinus);
                 if (nextOp != -1) {
                     if (commandQueue.size() < 3) {
-                        System.out.println("Inconsistent operation: " + commandQueue);
+                        msg = "Inconsistent operation: " + commandQueue;
+                        throw new IllegalStateException(msg);
                     }
                     computeAt(nextOp);
                 } else if (commandQueue.size() > 1) {
-                    System.out.println("Inconsistent state: " + commandQueue);
+                    msg = "Inconsistent state: " + commandQueue;
+                    throw new IllegalStateException(msg);
                 }
             }
         }
         final var finalResult = commandQueue.get(0);
         final var possibleException = nullIfNumberOrException(finalResult);
         if (possibleException != null) {
-            System.out.println("Invalid result of operation: " + finalResult);
+            msg = "Invalid result of operation: " + finalResult;
+            throw new IllegalStateException(msg, possibleException);
         }
         return finalResult;
         /*
@@ -96,20 +102,22 @@ public final class ArithmeticService {
 
     private void computeAt(final int operatorIndex) {
         if (operatorIndex == 0) {
-            System.out.println("Illegal start of operation: " + commandQueue);
+            msg = "Illegal start of operation: " + commandQueue;
+            throw new IllegalStateException(msg);
         }
         if (commandQueue.size() < 3) {
-            System.out.println("Not enough operands: " + commandQueue);
+            msg = "Not enough operands: " + commandQueue;
+            throw new IllegalStateException(msg);
         }
         if (commandQueue.size() < operatorIndex + 1) {
-            System.out.println("Missing right operand: " + commandQueue);
+            msg = "Missing right operand: " + commandQueue;
+            throw new IllegalStateException(msg);
         }
         final var rightOperand = commandQueue.remove(operatorIndex + 1);
         final var leftOperand = commandQueue.remove(operatorIndex - 1);
         if (KEYWORDS.contains(rightOperand) || KEYWORDS.contains(leftOperand)) {
-            System.out.println(
-                "Expected a number, but got " + leftOperand + " and " + rightOperand + " in " + commandQueue
-            );
+            msg = "Expected a number, but got " + leftOperand + " and " + rightOperand + " in " + commandQueue;
+            throw new IllegalStateException(msg);
         }
         final var right = parseDouble(rightOperand);
         final var left = parseDouble(leftOperand);
@@ -120,8 +128,8 @@ public final class ArithmeticService {
             case TIMES -> left * right;
             case DIVIDED -> left / right;
             default ->  {
-                System.out.println("Unknown operand " + operand);
-                yield Double.NaN;
+                msg = "Unknown operand " + operand;
+                throw new IllegalStateException(msg);
             }
         };
         commandQueue.set(operatorIndex - 1, Double.toString(result));
